d463f6804fd367933844bc5ba5615113ceb0cc03
Yna Owusu <ynaowusugmail.com@Ynas-MacBook-Air-2.local>
2024-12-06 16:54:04 -0500

    Initial commit

---
+++ Python-Projects/BandNameGenerator.py
@@ -0,0 +1,11 @@
+#create a greeting 
+# ask the user for the city that they grew up in and store it in a variable 
+# ask the user for the name of a pet and store it in a variable 
+# combine the name of their city and pet and show them their band name 
+# make sure the input cursor shows on a new line:
+
+print("Hello! Welcome to Band Name Generator ! Proceed to generate a name for your Band (:")
+city_name = input("What's the name of the city you grew up in? \n")
+pet_name = input("What's your pet's name? \n")
+
+print("Your band name could be " + city_name + " " +  pet_name)
