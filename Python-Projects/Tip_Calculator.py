da1f8c64fd07d84698c2db53b2d23692408db0d7
Yna Owusu <ynaowusugmail.com@Ynas-MacBook-Air-2.local>
2024-12-07 15:11:53 -0500

    Tip Calculator

--- Python-Projects/Tip_Calculator.py
+++ Python-Projects/Tip_Calculator.py
@@ -3,7 +3,7 @@
 
 print("Welcome to the tip calculator!")
 
-total_bill = int(input("What was the total bill? $"))
+total_bill = float(input("What was the total bill? $"))
 
 tip = int(input("How much tip would you like to give? \n 10,12, or 15? "))
 
